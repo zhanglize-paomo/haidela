616efc4193f3be0f40c9ab60bd629b0b9c94c507
zhanglize <Z18434395962@163.com>
2019-10-01 20:09:57 +0800

    update

---
+++ src/main/java/com/haidela/HaidelaApplication.java
@@ -0,0 +1,14 @@
+package com.haidela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaidelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaidelaApplication.class, args);
+		System.out.println("项目启动成功");
+	}
+
+}
