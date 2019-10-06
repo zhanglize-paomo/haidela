d5eab2f55aee7a6fd5f7e22ffd6f76b4781f6337
zhanglize <Z18434395962@163.com>
2019-10-06 15:47:41 +0800

    update

--- src/main/java/com/haidela/HaidelaApplication.java
+++ src/main/java/com/haidela/HaidelaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("com.haidela.payment")
+@ComponentScan(value = "com.haidela")
 @SpringBootApplication
 public class HaidelaApplication {
 
