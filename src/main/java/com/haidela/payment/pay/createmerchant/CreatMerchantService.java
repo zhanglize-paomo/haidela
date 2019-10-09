1566b32f4a0fcd54d70f970c110c09424599485e
zhanglize <Z18434395962@163.com>
2019-10-09 13:49:15 +0800

    update(签名):修改签名信息

--- src/main/java/com/haidela/payment/pay/createmerchant/CreatMerchantService.java
+++ src/main/java/com/haidela/payment/pay/createmerchant/CreatMerchantService.java
@@ -1,6 +1,7 @@
 package com.haidela.payment.pay.createmerchant;
 
 import com.haidela.payment.util.MD5;
+import com.haidela.payment.util.RSAUtils;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +17,9 @@ import java.util.TreeMap;
  */
 @Service
 public class CreatMerchantService {
-
+    String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDFf6krDWQUDUzKj+K+WvML2EyZLagKaJ5YTeCoBNhx/WpD1Vh2j/wQ9G3RC/" +
+            "tpUcmE7szr/vgdEVHkOfk6mGpeHapS6QE4enJ/CVaTPM573uI8VGWBek9v/E6HaVxRV0Hs8ZsvHAKopqYNDZRKhIrlLUrrkFD2KXJgIiRPQALeMQIDAQAB";
+    String key = "adc2fbfb4654ed95b28dfe0a0cb03da6";
     /**
      * 获取到参数对象的签名信息
      *
@@ -25,7 +28,6 @@ public class CreatMerchantService {
      * @return
      */
     public String getSign(HttpServletRequest request, HttpServletResponse response) {
-        String key = "adc2fbfb4654ed95b28dfe0a0cb03da6";
         Map<String, String> trnMap = new TreeMap<String, String>();
         trnMap.put("merchantName",request.getParameter("merchantName"));  //商户名称
         trnMap.put("merchantShortName",request.getParameter("merchantShortName"));    //商户简称
@@ -34,13 +36,13 @@ public class CreatMerchantService {
         trnMap.put("city",request.getParameter("city"));   //城市（地区CODE码）
         trnMap.put("county",request.getParameter("county"));   //城市（地区CODE码）
         trnMap.put("tel",request.getParameter("tel"));     //电话号
-        trnMap.put("email",request.getParameter("email"));  //邮箱   加密
-        trnMap.put("customerPhone",request.getParameter("customerPhone"));  //法人手机号  加密
+        trnMap.put("email", RSAUtils.encrypt(request.getParameter("email"), publicKey));  //邮箱   加密
+        trnMap.put("customerPhone",RSAUtils.encrypt(request.getParameter("customerPhone"), publicKey));  //法人手机号  加密
         trnMap.put("principal",request.getParameter("principal"));     //负责人
-        trnMap.put("principalMobile",request.getParameter("principalMobile"));  //负责人手机号  加密
-        trnMap.put("idCode",request.getParameter("idCode"));  //负责人证件号   加密
+        trnMap.put("principalMobile",RSAUtils.encrypt(request.getParameter("principalMobile"), publicKey));  //负责人手机号  加密
+        trnMap.put("idCode",RSAUtils.encrypt(request.getParameter("idCode"), publicKey));  //负责人证件号   加密
         trnMap.put("indentityPhoto",request.getParameter("indentityPhoto"));    //负责人证件照 上传正反面两张，以;分割(前面图片处理中有返回)
-        trnMap.put("accountCode",request.getParameter("accountCode"));      //银行卡号  加密
+        trnMap.put("accountCode",RSAUtils.encrypt(request.getParameter("accountCode"), publicKey));      //银行卡号  加密
         trnMap.put("bankId",request.getParameter("bankId"));       //开户银行 ID (取分支行联行号表.xlsx)
         trnMap.put("accountName",request.getParameter("accountName"));   //开户人
         trnMap.put("contactLine",request.getParameter("contactLine"));    // 联行号 (取分支行联行号表.xlsx)
@@ -48,19 +50,20 @@ public class CreatMerchantService {
         trnMap.put("bankBranchName",request.getParameter("bankBranchName"));     //开户支行名称 (取分支行联行号表.xlsx)
         trnMap.put("bankProvince",request.getParameter("bankProvince"));      //开户支行所在省 (取分支行联行号表.xlsx 中 省份ID)
         trnMap.put("bankCity",request.getParameter("bankCity"));     //开户支行所在市 (取分支行联行号表.xlsx 中 城市ID)
-        trnMap.put("idCard",request.getParameter("idCard"));    //持卡人身份证号  加密
-        trnMap.put("bankTel",request.getParameter("bankTel"));    //持卡人手机号  加密
+        trnMap.put("idCard",RSAUtils.encrypt(request.getParameter("idCard"), publicKey));    //持卡人身份证号  加密
+        trnMap.put("bankTel",RSAUtils.encrypt(request.getParameter("bankTel"), publicKey));    //持卡人手机号  加密
         trnMap.put("address",request.getParameter("address"));    //法人地址
         trnMap.put("bankAddress",request.getParameter("bankAddress"));  //持卡人地址
         trnMap.put("billRate",request.getParameter("billRate"));     //结算费率（信用卡手续费费率 （微信支付宝小额贷记卡 ） （单笔交易≤1000元）） 例 千分之 3 直接传3
         trnMap.put("licensePhoto",request.getParameter("licensePhoto"));
         trnMap.put("businessLicense",request.getParameter("businessLicense"));
-        String str ="";
+        String str= MD5.getSignMsg(trnMap, key);
+        String sign = "";
         try {
-            str = MD5.md5(trnMap.toString());
+            sign = MD5.md5(str);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return str;
+        return sign;
     }
 }
