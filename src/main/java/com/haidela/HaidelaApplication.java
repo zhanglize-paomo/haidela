9aa8b6ed91da76ce1d61f5bfb54da8545993095a
zhanglize <Z18434395962@163.com>
2019-10-10 10:14:00 +0800

    update(启动类):修改启动类的信息

--- src/main/java/com/haidela/HaidelaApplication.java
+++ src/main/java/com/haidela/HaidelaApplication.java
@@ -1,11 +1,13 @@
 package com.haidela;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(value = "com.haidela")
 @SpringBootApplication
+@MapperScan(value = "com.haidela")
 public class HaidelaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/haidela/payment/pay/configure/mapper/MerchantConfigureMapper.java
+++ src/main/java/com/haidela/payment/pay/configure/mapper/MerchantConfigureMapper.java
@@ -1,6 +1,5 @@
 package com.haidela.payment.pay.configure.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import javax.naming.NamingException;
@@ -11,7 +10,6 @@ import javax.naming.NamingException;
  * @author zhanglize
  * @create 2019/10/10
  */
-@Mapper
 @Repository
 public interface MerchantConfigureMapper {
 
