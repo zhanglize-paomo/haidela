282f7980b9aa91acfecd44e7d9226247232b95e1
zhanglize <Z18434395962@163.com>
2019-10-18 10:41:32 +0800

    add(定时任务):开启定时任务

--- src/main/java/com/haidela/HaidelaApplication.java
+++ src/main/java/com/haidela/HaidelaApplication.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @ComponentScan(value = "com.haidela")
 @SpringBootApplication
 @MapperScan(value = "com.haidela")
