d4b73d16c76737fd27ec62efa585ed2187a557a0
zhanglize <Z18434395962@163.com>
2019-10-21 14:01:44 +0800

    update(修改轮询池):修改轮询池定时任务

--- src/main/java/com/haidela/payment/util/ScheduledTasks.java
+++ src/main/java/com/haidela/payment/util/ScheduledTasks.java
@@ -29,6 +29,7 @@ public class ScheduledTasks {
      * 轮询池定时任务,每天晚上00:00对于轮询池中的状态,以及单日的交易金额总量进行清0
      */
     @Scheduled(cron = "0 0 0 * * ?")
+//    @Scheduled(cron = "1 * * * * ?")
     public void task() {
         logger.info("轮询池定时任务 :" + DateUtils.stringToDate());
         //查询所有状态为完成状态的1所对应的个体商户配置信息
@@ -39,7 +40,7 @@ public class ScheduledTasks {
             configureService.update(merchantConfigure.getId(), merchantConfigure.getTotalOneAmount(), merchantConfigure.getStatus());
         });
         //查询所有状态为未完成状态0所对应的个体工商户信息
-        List<MerchantConfigure> list = configureService.findByStstus("1");
+        List<MerchantConfigure> list = configureService.findByStstus("0");
         list.forEach(merchantConfigure -> {
             merchantConfigure.setTotalOneAmount("0");
             merchantConfigure.setStatus("0");
