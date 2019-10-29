13e9d7e6223753222188e4f87363b944487ce78c
zhanglize <Z18434395962@163.com>
2019-10-29 17:47:35 +0800

    update(新增):修改配置新增页面

--- webapp/js/date.js
+++
@@ -1,6 +0,0 @@
-function daysBetween(DateStart,DateEnd) {
-    var timestampStart = Date.parse(new Date(DateStart)) / 1000;
-    var timestampEnd = Date.parse(new Date(DateEnd)) / 1000;
-    var cha= (timestampEnd - timestampStart)/36000/24;
-    return Math.abs(cha);
-}
